1c0d2c0db93f8decce8c36d470cff80c8e4aa54d
salauer <stephenalauer@gmail.com>
2014-11-20 13:32:03 -0500

    Updated about text to include links to paper and Nick email. Plus added disclaimer.

--- ALERT/ui.R
+++ ALERT/ui.R
@@ -28,7 +28,11 @@ shinyUI(pageWithSidebar(
     
     helpText("Created by Stephen A Lauer and Nicholas G Reich"),
     
-    helpText(a("Send us your comments or feedback!", href="mailto:slauer@schoolph.umass.edu", target="_blank")),
+    helpText(a("Send us your comments or feedback!", href="mailto:nick@schoolph.umass.edu", target="_blank")),
+    
+    helpText(a("Read the Paper!", 
+               href="http://cid.oxfordjournals.org/content/early/2014/11/18/cid.ciu749.full.pdf", 
+               target="_blank")),
     
     helpText(a("ALERT on GitHub", href="https://github.com/nickreich/ALERT", target="_blank")),
     
@@ -38,8 +42,17 @@ shinyUI(pageWithSidebar(
   mainPanel(
     tabsetPanel(
       tabPanel("About",
-               helpText('The Above Local Elevated Respiratory illness Threshold (ALERT) algorithm provides a simple, easy-to-use tool for defining the onset of seasonal epidemics in a community (such as a city or a hospital) that systematically collects surveillance data on a particular disease. The ALERT algorithm was designed and validated with hospital surveillance data on influenza A virus. A description of the underlying methodology is currently under review.'),
-               helpText('The data provided in this app is real influenza A data that has been modified from its original source to mask the original data, as part of existing data sharing agreements. Users may use the “upload” box on the left-hand side of this page to upload their own data. To function properly, any uploaded dataset must have a column named “Date” and a column named “Cases."')),
+               helpText('This is the web app for the paper ',
+                        a('"Triggering Interventions for Influenza: The ALERT Algorithm"', 
+                          href = "http://cid.oxfordjournals.org/content/early/2014/11/18/cid.ciu749.full.pdf",
+                          target = "_blank"), 
+                        ' by Reich et al, published in Clinical Infectious Diseases in November 2014.'),
+               helpText('The Above Local Elevated Respiratory illness Threshold (ALERT) algorithm provides a simple, easy-to-use tool for defining the onset of seasonal epidemics in a community (such as a city or a hospital) that systematically collects surveillance data on a particular disease. The ALERT algorithm was designed and validated with hospital surveillance data on influenza A virus.'),
+               helpText('The data provided in this app is real influenza A data that has been modified from its original source to mask the original data, as part of existing data sharing agreements. Users may use the “upload” box on the left-hand side of this page to upload their own data. To function properly, any uploaded dataset must have a column named “Date” and a column named “Cases."'),
+               helpText("This project was motivated by the authors' work on the Respiratory Protection Effectiveness Clinical Trial (ResPECT) study. While the authors have worked hard to assure the stability and accuracy of results provided, they do not assume any legal liability or responsibility for the accuracy, completeness, or usefulness of any information provided by this Shiny web application. Individuals interested in using the app for decision making purposes are encouraged to ", 
+                        a("contact the developers", 
+                          href = "mailto:nick@schoolph.umass.edu", target="_blank"),
+                        ".")),
       tabPanel("Data Summary", 
                dataTableOutput("summary"), 
                tags$style(type="text/css", '#summary tfoot {display:none;}'), 
